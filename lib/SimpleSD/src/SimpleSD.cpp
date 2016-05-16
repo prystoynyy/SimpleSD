e89eb42a46bf3ed17438d8c57171b73f4f5fff4f
Vasyl Prystoynyy <vasyl.prystoynyy@astelit.ukr>
2016-05-16 18:11:44 +0300

    Add implementations for logs method

--- lib/SimpleSD/src/SimpleSD.cpp
+++ lib/SimpleSD/src/SimpleSD.cpp
@@ -1,25 +1,45 @@
 #include "SimpleSD.h"
 
-void SimpleSD::_writeLog(char* log){
-        if(!this->_isSerialInit) {
-                Serial.begin(9600);
+const int SimpleSD::CS_PIN = 4;
+
+void SimpleSD::writeLog(char* log){
+        if(!this->writeLog_) {
+                return;
         }
+        if(!this->isSerialInit_) {
+                Serial.begin(9600);
+                this->isSerialInit_ = true;
+        }
+        Serial.println(log);
 }
 
-SimpleSD::SimpleSD(): SimpleSD::SimpleSD(4){}
+SimpleSD::SimpleSD() : SimpleSD::SimpleSD(SimpleSD::CS_PIN){
+}
 
 SimpleSD::SimpleSD(int cs_pin){
-        this->_cs_pin = cs_pin;
-        this->_isInit = false;
-        this->_isSerialInit = false;
-        this->_isSerialInit =false;
+        this->cs_pin_ = cs_pin;
+        this->isInit_ = false;
+        this->writeLog_ = false;
+        this->isSerialInit_ =false;
 }
 bool SimpleSD::Init(){
-        this->_isInit = SD.begin(this->_cs_pin);
-        return SD.begin(this->_cs_pin);
+        this->isInit_ = SD.begin(this->cs_pin_);
+        return this->isInit_;
 }
-void ShowLog();
-void HideLog();
-char* ReadFile(char* fileName);
+
+bool SimpleSD::IsInit(){
+  return this->isInit_;
+}
+
+void SimpleSD::ShowLog(){
+  this->writeLog_ = true;
+}
+void SimpleSD::HideLog(){
+  this->writeLog_ = false;
+}
+char* SimpleSD::ReadFile(char* fileName){
+  char* resultText = new char[255];
+  return resultText;
+}
 bool WriteFile(char* fileName, char* data, bool override);
 bool CopyFile(char* orgFileName, char* copyFileName);
--- lib/SimpleSD/src/SimpleSD.h
+++ lib/SimpleSD/src/SimpleSD.h
@@ -3,18 +3,20 @@
 
 class SimpleSD{
 private:
-  int _cs_pin;
-  bool _writeLog;
-  bool _isInit;
-  bool _isSerialInit;
-  void _writeLog(char* log);
+  const static int CS_PIN;
+  int cs_pin_;
+  bool writeLog_;
+  bool isInit_;
+  bool isSerialInit_;
+  void writeLog(char* log);
 public:
   SimpleSD();
   SimpleSD(int cs_pin);
   bool Init();
+  bool IsInit();
   void ShowLog();
   void HideLog();
   char* ReadFile(char* fileName);
   bool WriteFile(char* fileName, char* data, bool override);
-  bool CopyFile(char* orgFileName, char* copyFileName)
-}
+  bool CopyFile(char* orgFileName, char* copyFileName);
+};
